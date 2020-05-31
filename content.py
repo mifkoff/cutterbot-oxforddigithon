26b9f17484ca14c3f684f14dcd9c555c6a4ae951
Дарья Счастная <coldrainbow@MacBook-Pro-Dara.local>
2020-05-31 14:32:27 +0300

    answers for messages were added

---
+++ content.py
@@ -0,0 +1,25 @@
+languages = ['ru', 'en', 'de', 'es', 'aa', 'ab', 'af', 'ak', 'sq', 'am', 'ar', 'an', 'hy', 'as', 'av', 'ae', 'ay', 'az',
+             'ba', 'bm', 'eu', 'be', 'bn', 'bh', 'bi', 'bo', 'bs', 'br', 'bg', 'my', 'ca', 'cs', 'ch', 'ce', 'zh', 'cu',
+             'cy', 'cs', 'da', 'de', 'dv', 'nl', 'dz', 'el', 'eo', 'et', 'eu', 'ee', 'fo', 'fa', 'fj', 'fi', 'fr', 'cv',
+             'fy', 'ff', 'Ga', 'gd', 'ga', 'gl', 'gv', 'el', 'gn', 'gu', 'ht', 'ha', 'he', 'hz', 'hi', 'ho', 'hr', 'kw',
+             'hu', 'hy', 'ig', 'is', 'io', 'ii', 'iu', 'ie', 'ia', 'id', 'ik', 'is', 'it', 'jv', 'ja', 'kl', 'kn', 'ks',
+             'ka', 'kr', 'kk', 'km', 'ki', 'rw', 'ky', 'kv', 'kg', 'ko', 'kj', 'ku', 'lo', 'la', 'lv', 'li', 'ln', 'lt',
+             'lb', 'lu', 'lg', 'mk', 'mh', 'ml', 'mi', 'mr', 'ms', 'Mi', 'mk', 'mg', 'mt', 'mn', 'mi', 'ms', 'my', 'na',
+             'nv', 'nr', 'nd', 'ng', 'ne', 'nl', 'nn', 'nb', 'no', 'oc', 'oj', 'or', 'om', 'os', 'pa', 'fa', 'pi', 'pl',
+             'pt', 'ps', 'qu', 'rm', 'ro', 'ro', 'rn', 'sg', 'sa', 'si', 'sk', 'sk', 'sl', 'se', 'sm', 'sn', 'sd', 'kw',
+             'so', 'st', 'es', 'sq', 'sc', 'sr', 'ss', 'su', 'sw', 'sv', 'ty', 'ta', 'tt', 'te', 'tg', 'tl', 'th', 'bo',
+             'ti', 'to', 'tn', 'ts', 'tk', 'tr', 'tw', 'ug', 'uk', 'ur', 'uz', 've', 'vi', 'vo', 'cy', 'wa', 'wo', 'xh',
+             'yi', 'yo', 'za', 'zh', 'zu']
+audio_file_name = 'article_t2s.mp3'
+url_expression = 'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+'
+emojies = '😄😇😍😌😎🤪🤩😏🤯🤬🥴🤤👊🤝🤞✊✌️🤘👌☝️🦄🙈⚡️🔥🎲🚀💡💰💣🔮🦠🎁🎉📚📝❤️💔💖☢️⛔️💯⚠️🍭🍺🌚🌝'
+welcome_message = 'Hi there! 👋' \
+                  'I am CutterBot but my target are articles and videos rather than people  😏' \
+                  'I think you know that every second a lot of new information is published which makes it impossible to read and watch everything.' \
+                  'I have a solution for you: send me a link for article or video, I will make summary and voice it so that it would be convinient for you to know information inside quickly' \
+                  'As an example, you can send me an article on Forbes, for example https://www.forbes.ru/billionaire-school/401131-kodeks-obshchezhitiya-kak-osnovateli-skyeng-ne-slomalis-na-starte'
+answer_for_message_wo_links = 'You message does not contain any URLs so that I cannot process it. 😔 ' \
+                              'Please, send your link again '
+answer_for_message_with_links = 'Got it! ' \
+                                'Will come back when I am done processing'
+
---
+++ main.py
@@ -0,0 +1,84 @@
+import traceback
+
+from functions import *
+import telebot
+import re
+import config
+#import database
+import content
+
+bot = telebot.TeleBot(config.telegram_access_token)
+
+
+def url_extractor(text):
+    return re.findall("(?P<url>https?://[^\s]+)", text)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    if database.is_user_new(user_id=message.chat.id):
+        database.new_user(user_id=message.chat.id)
+        bot.send_message(chat_id=message.chat.id, text=Welcome_message)  # welcome message
+        database.set_state(user_id=message.chat.id, state='waiting')
+    else:
+        bot.send_message(chat_id=message.chat.id, text='Already in bot')  # TODO change
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    if database.get_state(user_id=message.chat.id) != 'waiting':
+        text = message.text
+        urls = url_extractor(text)
+        if len(urls) == 0:
+            bot.reply_to(message, answer_for_message_wo_links)  # answer for messages w/o links
+        #     TODO all texts to content file
+        else:
+            database.set_state(user_id=message.chat.id, state='waiting')
+            bot.reply_to(message, answer_for_message_with_links)  # answer for links it gets
+            for url in urls:
+                if video_id_extractor(url):
+                    summary_text = get_video_summary(url)
+                    if summary_text is not False:
+                        bot.send_message(chat_id=message.chat.id, text='Uploading your video 🤪')
+                        video = open('final_video.mp4', 'rb')
+                        sended_message = bot.send_video(message.chat.id, video)
+                        database.new_summary(
+                            user_id=message.chat.id,
+                            message_id=sended_message.message_id,
+                            summary_text=summary_text,
+                            url=url,
+                            summary_type='video'
+                        )
+                    else:
+                        bot.send_message(chat_id=message.chat.id, text='cannot do that')
+                else:
+                    page_text = get_page_text(url)
+                    if len(page_text) != 0:
+                        summary_text = get_summary(page_text)
+                        text_to_speech(summary_text)
+                        sended_message = bot.reply_to(message, prettify_output(summary_text))
+                        # Audio
+                        audio = open(content.audio_file_name, 'rb')
+                        bot.send_audio(message.chat.id, audio)
+                        database.new_summary(
+                            user_id=message.chat.id,
+                            message_id=sended_message.message_id,
+                            summary_text=summary_text,
+                            url=url,
+                            summary_type='article'
+                        )
+                    else:
+                        bot.send_message(chat_id=message.chat.id, text='cannot do that')
+    else:
+        bot.send_message(chat_id=message.chat.id, text='Your summary is in progress')
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            prettify_output('Started')
+            bot.polling()
+        except ConnectionError:
+            print('ConnectionError', time.time())
+            print(traceback.format_exc())
+            time.sleep(2)
